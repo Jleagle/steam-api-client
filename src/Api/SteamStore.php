da2c431a09ec4b80365a2db60151ad6d43e1887c
Jleagle <jimeagle@gmail.com>
2015-07-31 23:21:15 +0100

    SteamStore service

---
+++ src/Api/SteamStore.php
@@ -0,0 +1,28 @@
+<?php
+namespace Jleagle\SteamClient\Api;
+
+use Jleagle\SteamClient\Exceptions\SteamException;
+use Jleagle\SteamClient\Responses\AppResponse;
+
+class SteamStore extends Steam
+{
+  /**
+   * @return string
+   */
+  protected function _getService()
+  {
+    return 'Store';
+  }
+
+  /**
+   * @param int $appId
+   * @return AppResponse
+   * @throws SteamException
+   */
+  public function appDetails($appId)
+  {
+    $data = $this->_get(null, ['appids' => $appId]);
+
+    return new AppResponse($data[$appId]['data']);
+  }
+}
