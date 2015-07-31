b9095d41db55ae29d79ddeb461a75a95fec341e8
Jleagle <jimeagle@gmail.com>
2015-07-31 23:21:50 +0100

    SteamNews service

---
+++ src/Api/SteamNews.php
@@ -0,0 +1,37 @@
+<?php
+namespace Jleagle\SteamClient\Api;
+
+use Jleagle\SteamClient\Responses\NewsResponse;
+
+class SteamNews extends Steam
+{
+  /**
+   * @return string
+   */
+  protected function _getService()
+  {
+    return 'ISteamNews';
+  }
+
+  /**
+   * @param int $appId
+   * @param int $count
+   * @param int $maxLength
+   *
+   * @return NewsResponse[]
+   */
+  public function getNewsforApp($appId, $count = null, $maxLength = null)
+  {
+    $path = 'GetNewsForApp/v2';
+    $query = ['appid' => $appId, 'count' => $count, 'maxlength' => $maxLength];
+
+    $data = $this->_get($path, $query);
+
+    $items = [];
+    foreach($data['appnews']['newsitems'] as $item)
+    {
+      $items[] = new NewsResponse($item);
+    }
+    return $items;
+  }
+}
